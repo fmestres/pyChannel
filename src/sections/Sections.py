9fba89d12c87d0298db0c3c039404417d9e7bac2
fmestres <fmestres@hotmail.com>
2022-02-15 10:22:34 -0300

    more sections features. Needs more testing

--- src/sections/Sections.py
+++ src/sections/Sections.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-import math
-from typing import Type
+from dataclasses import Field, dataclass
+from functools import cached_property
 import numpy as np
 
 @dataclass
@@ -10,25 +9,25 @@ class Section(ABC):
     #Maximum depth of the flow at the cross section
     flow_depth: float 
 
-    @property
+    @cached_property
     @abstractmethod
-    def area(self):
+    def area(self) -> float:
         #Cross section area
         pass
 
-    @property
+    @cached_property
     @abstractmethod
     def perimeter(self) -> float:
         #Wet perimeter
         pass
 
-    @property
+    @cached_property
     @abstractmethod
     def hydraulic_radius(self) -> float:
         #Quotient of area and perimeter. If peremeter is 0, it returns 0
         pass
 
-    @property
+    @cached_property
     @abstractmethod
     def centroid(self) -> tuple[float]:
         #Returns the tuple: (<x distance from centroid to leftmost point in the section>, <y depth of the centroid>)
@@ -38,26 +37,27 @@ class Section(ABC):
 @dataclass
 class RectangularSection(Section):
     
-    base_width: float
+    base_width: float 
 
-    @property
+    @cached_property
     def area(self) -> float:
         return self.base_width * self.flow_depth
     
-    @property
+    @cached_property
     def perimeter(self) -> float:
         return self.base_width + 2 * self.flow_depth
 
-    @property
+    @cached_property
     def hydraulic_radius(self) -> float:
         try:
             return self.area / self.perimeter
         except ZeroDivisionError:
             #if perimeter is 0, hydraulic_radius is set to 0
             return 0
-    @property
+
+    @cached_property
     def centroid(self) -> tuple[float, float]:
-        return (self.base_width / 2, self.flow_depth / 2)
+        return self.base_width / 2, self.flow_depth / 2
 
 
 @dataclass
@@ -65,33 +65,39 @@ class CircularSection(Section):
     
     radius: float
 
-    def __post_init__(self):
+
+    def __post_init__(self) -> None:
+
         if self.flow_depth > 2 * self.radius:
             raise ValueError('flow_depth cannot be greater than the available height (twice the radius of the cross section)')
-        
-    @property
-    def area(self) -> float:
+            
+        self.compute_central_angle()  
+
+    def compute_central_angle(self) -> None:
+        #Computes central angle of section. If radius is 0, central angle is set to 0
         try:
-            central_angle = 2 * math.acos((self.radius - self.flow_depth) / self.radius)
-
-            return 0.5 * self.radius ** 2 * (central_angle - np.sin(central_angle))
+            self.central_angle = 2 * np.arccos((self.radius - self.flow_depth) / self.radius, dtype=float)
         except ZeroDivisionError:
-            return 0
-    
-    @property
-    def perimeter(self) -> float:
-        return 2 * np.pi * self.radius
+            self.central_angle = 0
 
-    @property
+    @cached_property
+    def area(self) -> float:
+        return 0.5 * self.radius ** 2 * (self.central_angle - np.sin(self.central_angle, dtype=float))
+
+    @cached_property
+    def perimeter(self) -> float:
+        return 0.5 / np.pi * self.central_angle * self.radius
+
+    @cached_property
     def hydraulic_radius(self) -> float:
         try:
-            return self.area / self.perimeter
+            return float(self.area) / float(self.perimeter)
         except ZeroDivisionError:
-            #if perimeter is 0, hydraulic_radius is set to 0
             return 0
         
-    @property
+    @cached_property
     def centroid(self) -> tuple[float, float]:
+        
         return (self.radius, self.radius)
     
 
@@ -99,6 +105,42 @@ class CircularSection(Section):
 class TrapezoidalSection(Section):
     
     base_width: float
-    side_slope_1: float
-    side_slope_2: float
+    side_slope_1: float #left
+    side_slope_2: float 
 
+    @cached_property
+    def area(self) -> float:
+        return (self.base_width + 0.5 * self.flow_depth * (self.side_slope_1 + self.side_slope_2)) * self.flow_depth
+    
+    @cached_property
+    def perimeter(self) -> float:
+        return self.base_width + self.flow_depth * ((1 + self.side_slope_1 ** 2) ** 0.5 + (1 + self.side_slope_2 ** 2) ** 0.5)
+
+    @cached_property
+    def hydraulic_radius(self) -> float:
+        try:
+            return self.area / self.perimeter
+        except ZeroDivisionError:
+            return 0
+
+    @cached_property
+    def centroid(self) -> tuple[float, float]:
+        #Average positions of centroids of one rectangle and two triangles
+        rectangle_area = self.base_width * self.flow_depth
+        left_triangle_area = 0.5 * self.side_slope_1 * self.flow_depth ** 2
+        right_triangle_area = 0.5 * self.side_slope_2 * self.flow_depth ** 2
+        left_triangle_centroid_distance = 2 / 3 * self.side_slope_1 * self.flow_depth 
+        rectangle_centroid_distance = left_triangle_centroid_distance + self.base_width * 0.5
+        right_triangle_centroid_distance = rectangle_centroid_distance + 2 / 3 * self.side_slope_2 * self.flow_depth 
+
+        x_sum = left_triangle_area * left_triangle_centroid_distance + rectangle_area * rectangle_centroid_distance + right_triangle_area * right_triangle_centroid_distance
+        x_coord = x_sum / self.area
+
+        surface_width = self.base_width + self.flow_depth * (self.side_slope_1 + self.side_slope_2)
+        y_coord = self.flow_depth * (2 * self.base_width + surface_width) / (3 * (self.base_width + surface_width))
+        return x_coord, y_coord
+    
+
+trap = CircularSection(0, 0)
+print(trap.area, trap.hydraulic_radius)
+print(trap)
